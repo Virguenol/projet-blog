9d7e57f9f4a841b5586121fa1bc2e29aa36efd4a
Archange <virguenolngot@gmail.com>
2022-05-30 09:08:45 +0100

    les commentaites

--- app/Core/Calculator.php
+++ app/Core/Calculator.php
@@ -1,10 +1,6 @@
 <?php
 
 namespace App\Core;
-<<<<<<< HEAD
-// where is code comments????? :/
-// code not well formatted
-=======
 
 use Exception;
 /**
@@ -12,29 +8,21 @@ use Exception;
  *
  * class
  */
->>>>>>> a05477f (ajout des commentaire)
 class Calculator {
 
     public function __construct()
     {
 
     }
-<<<<<<< HEAD
-    // @FIXME in english the name of the vars
-    public function add(float $numero1, float $numero2){
-        return $numero1 + $numero2;
-=======
     /**
      * Addition function
      *
      * @param float $number1
      * @param float $number2
-     * @return void
      */
-    public function addition(float $number1, float $number2)
+    public function addition(float $number1, float $number2): float
     {
         return $number1 + $number2;
->>>>>>> a05477f (ajout des commentaire)
     }
 
     /**
@@ -44,24 +32,18 @@ class Calculator {
      * @param float $number2
      * @return void
      */
-    public function multiplication(float $number1, float $number2)
+    public function multiplication(float $number1, float $number2): float
     {
         return $number1 * $number2;
     }
-<<<<<<< HEAD
-    // @FIXME in english sub
-    public function sous(float $numero1, float $numero2){
-        return $numero1 - $numero2;
-=======
 
     /**
      * Subtraction function
      *
      * @param float $number1
      * @param float $number2
-     * @return void
      */
-    public function substraction(float $number1, float $number2)
+    public function substraction(float $number1, float $number2): float
     {
         return $number1 - $number2;
     }
@@ -71,9 +53,8 @@ class Calculator {
      *
      * @param float $number1
      * @param float $number2
-     * @return void
      */
-    public function division(float $number1, float $number2)
+    public function division(float $number1, float $number2): float
     {
         if($number2 == 0) {
            throw new Exception();
@@ -81,8 +62,6 @@ class Calculator {
             return $number1 / $number2;
         }
 
->>>>>>> a05477f (ajout des commentaire)
     }
 
-    // @FIXME and division????
 }
